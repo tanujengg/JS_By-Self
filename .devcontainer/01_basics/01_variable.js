cf88ac1e59b98df9fed34c9fe2c4da89033a0ddc
Tanuj <110276714+tanujengg@users.noreply.github.com>
2025-03-29 09:21:26 +0000

    push

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 45895
+let accountEmail = "tushar00@gmail.com"
+var accountPass = "987654321"
+accountCity = "Noida"
+
+console.table([accountId, accountEmail, accountPass, accountCity])
